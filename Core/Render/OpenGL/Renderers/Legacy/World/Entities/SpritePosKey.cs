9f09e10971c1ef815df934e6b73527eb92554cc8
Nick <stlaurent.nicholas@gmail.com>
2024-11-09 04:49:28 -0500

    ignore a sprite if it has already been rendered at this position

---
+++ Core/Render/OpenGL/Renderers/Legacy/World/Entities/SpritePosKey.cs
@@ -0,0 +1,32 @@
+﻿using Helion.Geometry.Vectors;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Helion.Render.OpenGL.Renderers.Legacy.World.Entities;
+
+public readonly struct SpritePosKey(Vec2D pos, int sprite)
+{
+    public readonly Vec2D Pos = pos;
+    public readonly int Sprite = sprite;
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Pos.X, Pos.Y, Sprite);
+    }
+
+    public override bool Equals([NotNullWhen(true)] object? obj)
+    {
+        var key = (SpritePosKey)obj!;
+        return Sprite == key.Sprite && key.Pos.X == Pos.X && key.Pos.Y == Pos.Y;
+    }
+
+    public static bool operator ==(SpritePosKey left, SpritePosKey right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(SpritePosKey left, SpritePosKey right)
+    {
+        return !(left == right);
+    }
+}
